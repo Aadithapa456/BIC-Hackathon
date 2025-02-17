da48188933a8d03ff81873b02454884c796babe9
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2025-02-17 15:07:18 +0545

    SB-BE-4: reporting model and routes

--- backend/routes/reporting.js
+++ backend/routes/reporting.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllReports,
+  getMyReports,
   createReport,
+  upDownVote,
 } = require("../controller/reportingController");
 
 router.route("/all").get(getAllReports);
+router.route("/my").get(getMyReports);
 router.route("/create").post(createReport);
+router.route("/vote").post(upDownVote);
 
 module.exports = router;
